b4b27b2b46f443cde68f54395cc6d7814213466f
Antimekumawat111 <aktank810@gmail.com>
2024-10-23 16:23:27 +0530

    edit file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import "./App.css";
-import CloseIcon from '@mui/icons-material/Close';
-import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
-import ArrowRightIcon from '@mui/icons-material/ArrowRight';
-import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from "@mui/icons-material/Close";
+import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
+import MenuIcon from "@mui/icons-material/Menu";
 
-import hanuman from "./images/hanuman_1.png"; 
-import kids from "./images/kids 3.png"; 
+import hanuman from "./images/hanuman_1.png";
+import kids from "./images/kids 3.png";
 import pottar from "./images/potter 1.png";
-// import holi from "./images/holi 1.png";
+import redlayer from "./images/Rectangle 6.png";
+import holi from "./images/holi 1.png";
 
 export default function App() {
   const [isOpen, setIsOpen] = useState(false);
@@ -53,10 +54,18 @@ export default function App() {
           <div className="rightContanir">
             {/* Tags for Desktop View */}
             <div className="tagsContanir">
-              <a href="#" className="atag">About us</a>
-              <a href="#" className="atag">Initiatives</a>
-              <a href="#" className="atag">Blog</a>
-              <a href="#" className="atag">Contact us</a>
+              <a href="#" className="atag">
+                About us
+              </a>
+              <a href="#" className="atag">
+                Initiatives
+              </a>
+              <a href="#" className="atag">
+                Blog
+              </a>
+              <a href="#" className="atag">
+                Contact us
+              </a>
             </div>
 
             {/* Sidebar Menu for Mobile View */}
@@ -65,10 +74,18 @@ export default function App() {
                 <a href="#" onClick={closeMenu}>
                   <CloseIcon id="closeBtn" />
                 </a>
-                <a href="#" className="showtag">About us</a>
-                <a href="#" className="showtag">Initiatives</a>
-                <a href="#" className="showtag">Blog</a>
-                <a href="#" className="showtag">Contact us</a>
+                <a href="#" className="showtag">
+                  About us
+                </a>
+                <a href="#" className="showtag">
+                  Initiatives
+                </a>
+                <a href="#" className="showtag">
+                  Blog
+                </a>
+                <a href="#" className="showtag">
+                  Contact us
+                </a>
               </div>
 
               {/* Menu Icon for Sidebar */}
@@ -85,11 +102,16 @@ export default function App() {
                 className="imgtag"
                 id="firstKidImage"
                 style={{
-                  maskImage: "linear-gradient(to left, rgba(0,0,0,0.5), rgba(0,0,0,0))",
-                  WebkitMaskImage: "linear-gradient(to left, rgba(0,0,0,0.5), rgba(0,0,0,0))",
+                  maskImage:
+                    "linear-gradient(to left, rgba(0,0,0,0.5), rgba(0,0,0,0))",
+                  WebkitMaskImage:
+                    "linear-gradient(to left, rgba(0,0,0,0.5), rgba(0,0,0,0))",
                   marginLeft: "2%",
                 }}
               />
+              <img src={kids} className="imgtag" />
+              <img src={kids} className="imgtag" />
+              <img src={kids} className="imgtag" />
               <img src={kids} className="imgtag" />
               <img src={kids} className="imgtag" />
               <img src={kids} className="imgtag" />
@@ -115,9 +137,64 @@ export default function App() {
             </div>
           </div>
         </div>
-        
+
+        <div className="pottarContanir">
+          <img src={pottar} alt="poter1" className="pottarImg" />
+          <img src={redlayer} alt="redLayer" className="red-layer" />
+        </div>
+
+
+        <div className={`overlayimages ${isOpen ? "hideOverlay" : ""}`}>
+  <div className="paragraph" style={{ marginRight: "5%" }}>
+    <h1 className="pgraph">सुविचार</h1>
+    <div
+      style={{
+        height: "2px",
+        width: "68%",
+        backgroundColor: "white",
+        margin: "10px 0",
+      }}
+    ></div>
+
+    <h1 className="pgraph">
+      रेत और मिट्टी से हम कहानियों को आकार देते हैं | हर बर्तन एक कृति है, जो
+      हाथों से बनी है और जुनून से तपाई गई है | स्वागत है उस दुनिया में, जहाँ
+      सृजन हमारा हुनर है और हर कृति में एक अनकही कहानी बसी है |
+    </h1>
+  </div>
+
+  <div className="paragraphImg">
+    <img src={kids} id="overlayKidImg" />
+  </div>
+</div>
+
+
+
+
+        <div className="footer">
+          <div className="footerHolyImg">
+            <img src={holi} alt="HolyImg" id="holyImg" />
+          </div>
+
+          <div className="foot">
+            <h4 className="footerData">What we do ?</h4>
+            <div
+              style={{
+                height: "3px",
+                width: "75%",
+                backgroundColor: "orangered",
+                margin: "10px 0",
+              }}
+            ></div>
+            <h4 className="footerData">
+              भजन मंडली दवारा हर मंगलवार या शनिवार को भजन का कार्यक्रम होता है,
+              जिसकी महिलाएं शामिल होती हैं | संस्थान में सभी हिंदू त्योहारों को
+              धूमधाम से मनाया जाता है | यहां महिला सशक्तिकरण, योग तथा अन्य
+              कल्याणकारी कार्य भी करवाये जाते हैं |
+            </h4>
+          </div>
+        </div>
       </div>
     </>
   );
 }
-
