21fdeb276c36036cf6c81d0b3e1589ae61bfce94
Antimekumawat111 <aktank810@gmail.com>
2024-10-25 14:40:23 +0530

    add carausal

--- src/App.js
+++ src/App.js
@@ -14,104 +14,163 @@ import holi from "./images/holi 1.png";
 export default function App() {
   return (
     <>
-    <div className="contanir">
-
-      <div className="mainContanir">
-        {/* Header Section */}
-        <div className="header">
-          <h5 id="headerheading">कुम्हार समाज सेवा संस्थान, झुंझुनूं</h5>
-        </div>
-
-        <div className="middleContanir">
-          {/* Hanuman Image */}
-          <div className="hanumanImage">
-            <img src={hanuman} alt="hanuman" id="hanumanImage" />
+      <div className="contanir">
+        <div className="mainContanir">
+          {/* Header Section */}
+          <div className="header">
+            <h5 id="headerheading">कुम्हार समाज सेवा संस्थान, झुंझुनूं</h5>
           </div>
 
-          <div className="rightContanir">
-            <div className="tagsContanir">
-              <a href="#" className="atag">
-                About us
-              </a>
-              <a href="#" className="atag">
-                Initiatives
-              </a>
-              <a href="#" className="atag">
-                Blog
-              </a>
-              <a href="#" className="atag">
-                Contact us
-              </a>
+          <div className="middleContanir">
+            {/* Hanuman Image */}
+            <div className="hanumanImage">
+              <img src={hanuman} alt="hanuman" id="hanumanImage" />
             </div>
 
-            <div className="imageContanir">
-              <p id="p">भजन मंडली की झलकियां</p>
-              <img src={kids} className="imgtag" id="firstKidImage" />
-              <img src={kids} className="imgtag" id="secondKidImage" />
-              <img src={kids} className="imgtag" id="thirdKidImg" />
-              <img src={kids} className="imgtag" id="fouthKidImg" />
-              <img src={kids} className="imgtag" id="fifthKidImg" />
-              <img src={kids} className="imgtag" id="sixthKidImg" />
+            <div className="rightContanir">
+              <div className="tagsContanir">
+                <a href="#" className="atag">
+                  About us
+                </a>
+                <a href="#" className="atag">
+                  Initiatives
+                </a>
+                <a href="#" className="atag">
+                  Blog
+                </a>
+                <a href="#" className="atag">
+                  Contact us
+                </a>
+              </div>
+
+                <p id="p">भजन मंडली की झलकियाँ</p>
+              <div className="carousel">
+                <div className="ca">
+                  <img src={kids} className="imgtag" id="first" alt="Kid 1" />
+                  <img src={kids} className="imgtag" id="second" alt="Kid 2" />
+                  <img src={kids} className="imgtag" id="third" alt="Kid 3" />
+                  <img src={kids} className="imgtag" id="fourth" alt="Kid 4" />
+                  <img src={kids} className="imgtag" id="fifth" alt="Kid 5" />
+                  <img src={kids} className="imgtag" id="sixth" alt="Kid 6" />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="overlayimages">
+            <div className="pottarContanir">
+              <img src={pottar} alt="poter1" className="pottarImg" />
+              <img src={redlayer} alt="redLayer" className="red-layer" />
+            </div>
+
+            <div className="paragraph">
+              <h1>सुविचार</h1>
+              <div
+                style={{
+                  height: "2px",
+                  width: "68%",
+                  backgroundColor: "white",
+                  margin: "10px 0",
+                }}
+              ></div>
+
+              <h1>
+                रेत और मिट्टी से हम कहानियों को आकार देते हैं | हर बर्तन एक कृति
+                है, जो हाथों से बनी है और जुनून से तपाई गई है | स्वागत है उस
+                दुनिया में, जहाँ सृजन हमारा हुनर है और हर कृति में एक अनकही
+                कहानी बसी है |
+              </h1>
+            </div>
+            <div className="paragraphImg">
+              <img src={kids} id="overlayKidImg" className="pImg" />
             </div>
           </div>
         </div>
 
-        <div className="overlayimages">
-          <div className="pottarContanir">
-            <img src={pottar} alt="poter1" className="pottarImg" />
-            <img src={redlayer} alt="redLayer" className="red-layer" />
+        <div className="footer">
+          <div className="footerHolyImg">
+            <img src={holi} alt="HolyImg" id="holyImg" />
           </div>
 
-          <div className="paragraph" >
-            <h1>सुविचार</h1>
+          <div className="foot">
+            <h4 className="footerparegraph">What we do ?</h4>
             <div
               style={{
                 height: "2px",
                 width: "68%",
-                backgroundColor: "white",
+                backgroundColor: "#F97300",
                 margin: "10px 0",
               }}
             ></div>
-
-            <h1 >
-              रेत और मिट्टी से हम कहानियों को आकार देते हैं | हर बर्तन एक कृति
-              है, जो हाथों से बनी है और जुनून से तपाई गई है | स्वागत है उस
-              दुनिया में, जहाँ सृजन हमारा हुनर है और हर कृति में एक अनकही कहानी
-              बसी है |
-            </h1>
-          </div>
-          <div className="paragraphImg">
-            <img src={kids} id="overlayKidImg" className="pImg" />
+            <h4 className="footerparegraph">
+              भजन मंडली दवारा हर मंगलवार या शनिवार को भजन का कार्यक्रम होता है,
+              जिसकी महिलाएं शामिल होती हैं | संस्थान में सभी हिंदू त्योहारों को
+              धूमधाम से मनाया जाता है | यहां महिला सशक्तिकरण, योग तथा अन्य
+              कल्याणकारी कार्य भी करवाये जाते हैं |
+            </h4>
           </div>
         </div>
       </div>
-
-      <div className="footer">
-        <div className="footerHolyImg">
-          <img src={holi} alt="HolyImg" id="holyImg" />
-        </div>
-
-        <div className="foot">
-          <h4 className="footerparegraph">What we do ?</h4>
-          <div
-            style={{
-              height: "2px",
-                width: "68%",
-                backgroundColor: "#F97300",
-                margin: "10px 0",
-            }}
-          ></div>
-          <h4 className="footerparegraph">
-            भजन मंडली दवारा हर मंगलवार या शनिवार को भजन का कार्यक्रम होता है,
-            जिसकी महिलाएं शामिल होती हैं | संस्थान में सभी हिंदू त्योहारों को
-            धूमधाम से मनाया जाता है | यहां महिला सशक्तिकरण, योग तथा अन्य
-            कल्याणकारी कार्य भी करवाये जाते हैं |
-          </h4>
-        </div>
-      </div>
-    </div>
-
-
     </>
   );
 }
+
+<div className="carousel">
+  <div className="ca">
+    <img src={kids} className="imgtag" id="first" alt="Kid 1" />
+    <img src={kids} className="imgtag" id="second" alt="Kid 2" />
+    <img src={kids} className="imgtag" id="third" alt="Kid 3" />
+    <img src={kids} className="imgtag" id="fourth" alt="Kid 4" />
+    <img src={kids} className="imgtag" id="fifth" alt="Kid 5" />
+    <img src={kids} className="imgtag" id="sixth" alt="Kid 6" />
+  </div>
+</div>;
+
+/*
+
+
+
+
+
+.carousel {
+  width: 80%;
+  overflow: hidden;
+  display: flex;
+  margin-left: auto;
+  justify-content: center;
+  position: relative;
+  border: 2px solid black;
+}
+
+.ca {
+  display: flex;
+  width: calc(173px * 7); 
+  animation: scroll 20s linear infinite; 
+}
+
+.ca img {
+  width: 173px;
+  height: 77px;
+  margin-right: 20px; 
+  border-radius: 10px;
+}
+
+@keyframes scroll {
+  0% {
+    transform: translateX(100%);
+  }
+  100% {
+    transform: translateX(-100%);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+*/
